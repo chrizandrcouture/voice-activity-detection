045f8074d2f27a9bb81f2334f3620453e42813b3
Chris <chris@Chriss-MacBook-Air.local>
2022-03-08 09:09:58 +0530

    Updates

---
+++ data.py
@@ -0,0 +1,368 @@
+# Specify the desired WAV-format.
+import python_speech_features
+import h5py_cache
+import array
+import webrtcvad
+import numpy as np
+import h5py
+import soundfile as sf
+from pydub import AudioSegment
+import re
+import glob
+
+
+SAMPLE_RATE = 16000
+SAMPLE_CHANNELS = 1
+SAMPLE_WIDTH = 2
+# Name of folder to save the data files in.
+DATA_FOLDER = 'data'
+# Min/max length for slicing the voice files.
+SLICE_MIN_MS = 1000
+SLICE_MAX_MS = 5000
+# Frame size to use for the labelling.
+FRAME_SIZE_MS = 30
+# Convert slice ms to frame size.
+SLICE_MIN = int(SLICE_MIN_MS / FRAME_SIZE_MS)
+SLICE_MAX = int(SLICE_MAX_MS / FRAME_SIZE_MS)
+
+# Calculate frame size in data points.
+FRAME_SIZE = int(SAMPLE_RATE * (FRAME_SIZE_MS / 1000.0))
+
+
+class FileManager:
+    '''
+    Keeps track of audio-files from a data-set.
+    Provides support for formatting the wav-files into a desired format.
+    Also provides support for conversion of .flac files (as we have in the LibriSpeech data-set).
+    '''
+
+    def __init__(self, name, directory):
+
+        self.name = name
+        self.data = h5py.File(DATA_FOLDER + '/' + name + '.hdf5', 'a')
+
+        # Setup file names.
+        if 'files' not in self.data:
+
+            # Get files.
+            files = glob.glob(directory + '/**/*.wav', recursive=True)
+            files.extend(glob.glob(directory + '/**/*.flac', recursive=True))
+            files = [f for f in files]
+
+            # Setup data set.
+            dt = h5py.special_dtype(vlen=str)
+            self.data.create_dataset('files', (len(files),), dtype=dt)
+
+            # Add file names.
+            for i, f in enumerate(files):
+                self.data['files'][i] = f
+
+    def get_track_count(self):
+        return len(self.data['files'])
+
+    def prepare_files(self, normalize=False):
+        '''
+        Prepares the files for the project.
+        Will do the following check for each file:
+        1. Check if it has been converted already to the desired format.
+        2. Converts all files to WAV with the desired properties.
+        3. Stores the converted files in a separate folder.
+        '''
+
+        if not OBJ_PREPARE_AUDIO:
+            print(f'Skipping check for {self.name}.')
+            return
+
+        print('Found {0} tracks to check.'.format(self.get_track_count()))
+        progress = 1
+
+        # Setup raw data set.
+        if 'raw' not in self.data:
+            dt = h5py.special_dtype(vlen=np.dtype(np.int16))
+            self.data.create_dataset(
+                'raw', (self.get_track_count(),), dtype=dt)
+
+        # Convert files to desired format and save raw content.
+        for i, file in enumerate(self.data['files']):
+
+            print('Processing {0} of {1}'.format(
+                progress, self.get_track_count()), end='\r', flush=True)
+            progress += 1
+
+            # Already converted?
+            if len(self.data['raw'][i]) > 0:
+                continue
+
+            # Convert file.
+            track = (AudioSegment.from_file(file)
+                     .set_frame_rate(SAMPLE_RATE)
+                     .set_sample_width(SAMPLE_WIDTH)
+                     .set_channels(SAMPLE_CHANNELS))
+
+            # Normalize?
+            if normalize:
+                track = track.apply_gain(-track.max_dBFS)
+
+            # Store data.
+            self.data['raw'][i] = np.array(
+                track.get_array_of_samples(), dtype=np.int16)
+
+        self.data.flush()
+        print('\nDone!')
+
+    def collect_frames(self):
+        '''
+        Takes all the audio files and merges their frames together into one long array
+        for use with the sample generator.
+        '''
+
+        if 'frames' in self.data:
+            print('Frame merging already done. Skipping.')
+            return
+
+        if 'raw' not in self.data:
+            print('Could not find raw data!')
+            return
+
+        frame_count = 0
+        progress = 1
+
+        # Calculate number of frames needed.
+        for raw in self.data['raw']:
+            frame_count += int((len(raw) + (FRAME_SIZE -
+                               (len(raw) % FRAME_SIZE))) / FRAME_SIZE)
+            print('Counting frames ({0} of {1})'.format(
+                progress, self.get_track_count()), end='\r', flush=True)
+            progress += 1
+
+        # Create data set for frames.
+        dt = np.dtype(np.int16)
+        self.data.create_dataset('frames', (frame_count, FRAME_SIZE), dtype=dt)
+
+        progress = 0
+
+        # Buffer to speed up merging as HDF5 is not fast with lots of indexing.
+        buffer = np.array([])
+        buffer_limit = FRAME_SIZE * 4096
+
+        # Merge frames.
+        for raw in self.data['raw']:
+
+            # Setup raw data with zero padding on the end to fit frame size.
+            raw = np.concatenate(
+                (raw, np.zeros(FRAME_SIZE - (len(raw) % FRAME_SIZE))))
+
+            # Add to buffer.
+            buffer = np.concatenate((buffer, raw))
+
+            # If buffer is not filled up and we are not done, keep filling the buffer up.
+            if len(buffer) < buffer_limit and progress + (len(buffer) / FRAME_SIZE) < frame_count:
+                continue
+
+            # Get frames.
+            frames = np.array(np.split(buffer, len(buffer) / FRAME_SIZE))
+            buffer = np.array([])
+
+            # Add frames to list.
+            self.data['frames'][progress: progress + len(frames)] = frames
+
+            progress += len(frames)
+            print('Merging frames ({0} of {1})'.format(
+                progress, frame_count), end='\r', flush=True)
+
+        self.data.flush()
+        print('\nDone!')
+
+    def label_frames(self):
+        '''
+        Takes all audio frames and labels them using the WebRTC VAD.
+        '''
+
+        if 'labels' in self.data:
+            print('Frame labelling already done. Skipping.')
+            return
+
+        if 'frames' not in self.data:
+            print('Could not find any frames!')
+            return
+
+        vad = webrtcvad.Vad(0)
+
+        frame_count = len(self.data['frames'])
+        progress = 0
+        batch_size = 65536
+
+        # Create data set for labels.
+        dt = np.dtype(np.uint8)
+        self.data.create_dataset('labels', (frame_count,), dtype=dt)
+
+        # Label all the frames.
+        for pos in range(0, frame_count, batch_size):
+
+            frames = self.data['frames'][pos: pos + batch_size]
+            labels = [1 if vad.is_speech(
+                f.tobytes(), sample_rate=SAMPLE_RATE) else 0 for f in frames]
+            self.data['labels'][pos: pos + batch_size] = np.array(labels)
+
+            progress += len(labels)
+            print('Labelling frames ({0} of {1})'.format(
+                progress, frame_count), end='\r', flush=True)
+
+        self.data.flush()
+        print('\nDone!')
+
+
+def add_noise(speech_frames, noise_frames, align_frames, noise_level_db):
+
+    # Convert to tracks.
+    speech_track = (AudioSegment(data=array.array('h', speech_frames.flatten()),
+                                 sample_width=SAMPLE_WIDTH, frame_rate=SAMPLE_RATE,
+                                 channels=SAMPLE_CHANNELS))
+
+    noise_track = (AudioSegment(data=array.array('h', noise_frames.flatten()),
+                                sample_width=SAMPLE_WIDTH, frame_rate=SAMPLE_RATE,
+                                channels=SAMPLE_CHANNELS))
+
+    # Overlay noise.
+    track = noise_track.overlay(
+        speech_track, gain_during_overlay=noise_level_db)
+
+    # Get frames data from track.
+    raw = np.array(track.get_array_of_samples(), dtype=np.int16)
+    frames = np.array(np.split(raw, len(raw) / FRAME_SIZE))
+
+    # Add previous frames to align MFCC window.
+    frames_aligned = np.concatenate((align_frames, frames))
+
+    mfcc = python_speech_features.mfcc(frames_aligned, SAMPLE_RATE, winstep=(FRAME_SIZE_MS / 1000),
+                                       winlen=mfcc_window_frame_size * (FRAME_SIZE_MS / 1000), nfft=2048)
+
+    # First MFCC feature is just the DC offset.
+    mfcc = mfcc[:, 1:]
+    delta = python_speech_features.delta(mfcc, 2)
+
+    return frames, mfcc, delta
+
+if __name__ == "__main__":
+    print('Loading files...')
+    speech_dataset = FileManager('speech', 'LibriSpeech')
+
+    noise_dataset = FileManager('noise', 'QUT-NOISE')
+
+    speech_dataset.prepare_files()
+    noise_dataset.prepare_files(normalize=True)
+
+    print('Collecting frames...')
+
+    speech_dataset.collect_frames()
+    noise_dataset.collect_frames()
+
+    print('Labelling frames...')
+    speech_dataset.label_frames()
+    data = h5py_cache.File(DATA_FOLDER + '/data.hdf5', 'a',
+                           chunk_cache_mem_size=1024**3)
+    noise_levels_db = {'None': None, '-15': -15, '-3': -3}
+
+    mfcc_window_frame_size = 4
+
+    speech_data = speech_dataset.data
+    noise_data = noise_dataset.data
+
+    np.random.seed(1337)
+
+    if 'labels' not in data:
+
+        print('Shuffling speech data and randomly adding 50% silence.')
+
+        pos = 0
+        l = len(speech_dataset.data['frames'])
+        slices = []
+
+        # Split speech data randomly within the given slice length.
+        while pos + SLICE_MIN < l:
+            slice_indexing = (
+                pos, pos + np.random.randint(SLICE_MIN, SLICE_MAX + 1))
+            slices.append(slice_indexing)
+            pos = slice_indexing[1]
+
+        # Add remainder to last slice.
+        slices[-1] = (slices[-1][0], l)
+
+        pos = 0
+
+        # Add random silence (50%) to the track within the given slice length.
+        while pos + SLICE_MIN < l:
+            length = np.random.randint(SLICE_MIN, SLICE_MAX + 1)
+            slice_indexing = (length, length)
+            slices.append(slice_indexing)
+            pos += length
+
+        # Get total frame count.
+        total = l + pos + mfcc_window_frame_size
+
+        # Shuffle the content randomly.
+        np.random.shuffle(slices)
+
+        # Create data set for input.
+        for key in noise_levels_db:
+            data.create_dataset(
+                'frames-' + key, (total, FRAME_SIZE), dtype=np.dtype(np.int16))
+            data.create_dataset('mfcc-' + key, (total, 12),
+                                dtype=np.dtype(np.float32))
+            data.create_dataset('delta-' + key, (total, 12),
+                                dtype=np.dtype(np.float32))
+
+        # Create data set for labels.
+        dt = np.dtype(np.int8)
+        data.create_dataset('labels', (total,), dtype=dt)
+
+        pos = 0
+
+        # Construct speech data.
+        for s in slices:
+
+            # Silence?
+            if s[0] == s[1]:
+                frames = np.zeros((s[0], FRAME_SIZE))
+                labels = np.zeros(s[0])
+            # Otherwise use speech data.
+            else:
+                frames = speech_data['frames'][s[0]: s[1]]
+                labels = speech_data['labels'][s[0]: s[1]]
+
+            # Pick random noise to add.
+            i = np.random.randint(0, len(noise_data['frames']) - len(labels))
+            noise = noise_data['frames'][i: i + len(labels)]
+
+            # Setup noise levels.
+            for key in noise_levels_db:
+
+                # Get previous frames to align MFCC window with new data.
+                if pos == 0:
+                    align_frames = np.zeros(
+                        (mfcc_window_frame_size - 1, FRAME_SIZE))
+                else:
+                    align_frames = data['frames-' + key][pos -
+                                                         mfcc_window_frame_size + 1: pos]
+
+                # Add noise and get frames, MFCC and delta of MFCC.
+                frames, mfcc, delta = add_noise(np.int16(frames), np.int16(noise),
+                                                np.int16(align_frames), noise_levels_db[key])
+
+                data['frames-' + key][pos: pos + len(labels)] = frames
+                data['mfcc-' + key][pos: pos + len(labels)] = mfcc
+                data['delta-' + key][pos: pos + len(labels)] = delta
+
+            # Add labels.
+            data['labels'][pos: pos + len(labels)] = labels
+
+            pos += len(labels)
+            print('Generating data ({0:.2f} %)'.format(
+                (pos * 100) / total), end='\r', flush=True)
+
+        data.flush()
+
+        print('\nDone!')
+
+    else:
+        print('Speech data already generated. Skipping.')
